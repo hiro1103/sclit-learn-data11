9bd5528c0cb79a96aebc629b3e675cf44465bd01
murakami <96195951+hiro1103@users.noreply.github.com>
2022-04-17 02:33:20 +0900

    Add files via upload

--- data11.py
+++ data11.py
@@ -1,10 +1,18 @@
-from cProfile import label
 from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
 from matplotlib import cm
 from sklearn.metrics import silhouette_samples
+from scipy.spatial.distance import pdist, squareform
+from scipy.cluster.hierarchy import linkage
+from scipy.cluster.hierarchy import dendrogram
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.datasets import make_moons
+from sklearn.cluster import DBSCAN
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import warnings
+warnings.simplefilter('ignore')
 
 # データ点の個数：n_samples
 # 特徴量の個数：n_features
@@ -26,13 +34,8 @@ plt.scatter(X[:, 0], X[:, 1],
 plt.grid()
 plt.tight_layout()
 plt.show()
-# クラスタの個数
-# セントロイドの初期値をランダムに選択
-# 異なるセントロイドの初期値を用いたk-meansアルゴリズムの実行回数
-# k-meansアルゴリズム内部の最大イテレション回数
-# 収束と判定するための相対的な許容誤差
-# セントロイドの初期化に用いる乱数生成器の状態
-# クラスタ中心の計算と各データ点のインデックスの予測
+
+
 km = KMeans(n_clusters=3,
             init='random',
             n_init=10,
@@ -70,6 +73,7 @@ plt.show()
 
 print('Distortion: %.2f' % km.inertia_)
 
+
 distortions = []
 for i in range(1, 11):
     km = KMeans(n_clusters=i,
@@ -79,7 +83,6 @@ for i in range(1, 11):
                 random_state=0)
     km.fit(X)
     distortions.append(km.inertia_)
-
 plt.plot(range(1, 11), distortions, marker='o')
 plt.xlabel('Number of clusters')
 plt.ylabel('Distortion')
@@ -92,40 +95,33 @@ km = KMeans(n_clusters=3,
             max_iter=300,
             tol=1e-04,
             random_state=0)
+y_km = km.fit_predict(X)
 
 cluster_labels = np.unique(y_km)
 n_clusters = cluster_labels.shape[0]
-# シルエット係数を計算
 silhouette_vals = silhouette_samples(X, y_km, metric='euclidean')
 y_ax_lower, y_ax_upper = 0, 0
 yticks = []
 for i, c in enumerate(cluster_labels):
-    c_silhouseette_vals = silhouette_vals[y_km == c]
-    c_silhouseette_vals.sort()
-    y_ax_upper += len(c_silhouseette_vals)
-    # 色の値をセット
-    color = cm.jet(float(i)/n_clusters)
-    plt.barh(range(y_ax_lower, y_ax_upper),
-             c_silhouseette_vals,
-             height=1.0,
-             edgecolor='none',
-             color=color)
-    # クラスタラベルの表示位置を追加
-    yticks.append(y_ax_lower + y_ax_upper / 2.0)
-    # 底辺に値に棒の幅を追加
-    y_ax_lower += len(c_silhouseette_vals)
+    c_silhouette_vals = silhouette_vals[y_km == c]
+    c_silhouette_vals.sort()
+    y_ax_upper += len(c_silhouette_vals)
+    color = cm.jet(float(i) / n_clusters)
+    plt.barh(range(y_ax_lower, y_ax_upper), c_silhouette_vals, height=1.0,
+             edgecolor='none', color=color)
 
-# シルエット係数の平均値
+    yticks.append((y_ax_lower + y_ax_upper) / 2.)
+    y_ax_lower += len(c_silhouette_vals)
+
 silhouette_avg = np.mean(silhouette_vals)
-# 係数の平均値に破線を引く
 plt.axvline(silhouette_avg, color="red", linestyle="--")
-# クラスタラベルを表示
-plt.yticks(yticks, cluster_labels+1)
+
+plt.yticks(yticks, cluster_labels + 1)
 plt.ylabel('Cluster')
 plt.xlabel('Silhouette coefficient')
+
 plt.tight_layout()
 plt.show()
-
 km = KMeans(n_clusters=2,
             init='k-means++',
             n_init=10,
@@ -133,30 +129,170 @@ km = KMeans(n_clusters=2,
             tol=1e-04,
             random_state=0)
 y_km = km.fit_predict(X)
+
 plt.scatter(X[y_km == 0, 0],
             X[y_km == 0, 1],
             s=50,
             c='lightgreen',
-            edgecolors='black',
+            edgecolor='black',
             marker='s',
             label='Cluster 1')
-
 plt.scatter(X[y_km == 1, 0],
             X[y_km == 1, 1],
             s=50,
             c='orange',
-            edgecolors='black',
+            edgecolor='black',
             marker='o',
             label='Cluster 2')
 
-plt.scatter(km.cluster_centers_[:, 0],
-            km.cluster_centers_[:, 1],
-            s=250,
-            marker='*',
-            c='red',
-            label='Centroids')
-
+plt.scatter(km.cluster_centers_[:, 0], km.cluster_centers_[:, 1],
+            s=250, marker='*', c='red', label='Centroids')
 plt.legend()
 plt.grid()
+plt.tight_layout()
+plt.show()
+
+
+cluster_labels = np.unique(y_km)
+n_clusters = cluster_labels.shape[0]
+silhouette_vals = silhouette_samples(X, y_km, metric='euclidean')
+y_ax_lower, y_ax_upper = 0, 0
+yticks = []
+for i, c in enumerate(cluster_labels):
+    c_silhouette_vals = silhouette_vals[y_km == c]
+    c_silhouette_vals.sort()
+    y_ax_upper += len(c_silhouette_vals)
+    color = cm.jet(float(i) / n_clusters)
+    plt.barh(range(y_ax_lower, y_ax_upper), c_silhouette_vals, height=1.0,
+             edgecolor='none', color=color)
+
+    yticks.append((y_ax_lower + y_ax_upper) / 2.)
+    y_ax_lower += len(c_silhouette_vals)
+
+silhouette_avg = np.mean(silhouette_vals)
+plt.axvline(silhouette_avg, color="red", linestyle="--")
+
+plt.yticks(yticks, cluster_labels + 1)
+plt.ylabel('Cluster')
+plt.xlabel('Silhouette coefficient')
+
+plt.tight_layout()
+plt.show()
+
+np.random.seed(123)
+variables = ['X', 'Y', 'Z']
+labels = ['ID_0', 'ID_1', 'ID_2', 'ID_3', 'ID_4']
+X = np.random.random_sample([5, 3])*10
+df = pd.DataFrame(X, columns=variables, index=labels)
+df
+
+row_dist = pd.DataFrame(squareform(pdist(df, metric='euclidean')),
+                        columns=labels, index=labels)
+
+# help(linkage)
+row_clusters = linkage(row_dist, method='complete', metric='euclidean')
+row_clusters = linkage(pdist(df, metric='euclidean'), method='complete')
+row_clusters = linkage(df.values, method='complete', metric='euclidean')
+
+pd.DataFrame(row_clusters,
+             columns=['row label 1', 'row label 2',
+                      'distance', 'no. of items in clust.'],
+             index=['cluster %d' % (i+1) for i in range(row_clusters.shape[0])])
+
+# 樹形図を黒で表示する場合
+row_dendr = dendrogram(row_clusters,
+                       labels=labels)
+plt.ylabel('Euclidean distance')
+plt.tight_layout()
+plt.show()
+
+fig = plt.figure(figsize=(8, 8), facecolor='white')
+axd = fig.add_axes([0.09, 0.1, 0.2, 0.6])
+
+row_dendr = dendrogram(row_clusters, orientation='left')
+df_rowclust = df.iloc[row_dendr['leaves'][::-1]]
+
+axd.set_xticks([])
+axd.set_yticks([])
+
+for i in axd.spines.values():
+    i.set_visible(False)
+
+axm = fig.add_axes([0.23, 0.1, 0.6, 0.6])  # x-pos, y-pos, width, height
+cax = axm.matshow(df_rowclust, interpolation='nearest', cmap='hot_r')
+fig.colorbar(cax)
+axm.set_xticklabels([''] + list(df_rowclust.columns))
+axm.set_yticklabels([''] + list(df_rowclust.index))
+
+plt.show()
+
+ac = AgglomerativeClustering(n_clusters=3,
+                             affinity='euclidean',
+                             linkage='complete')
+
+labels = ac.fit_predict(X)
+print('Cluster labels: %s' % labels)
+
+ac = AgglomerativeClustering(
+    n_clusters=2, affinity='euclidean', linkage='complete')
+labels = ac.fit_predict(X)
+print('Cluster labels: %s' % labels)
+
+X, y = make_moons(n_samples=200,
+                  noise=0.05,
+                  random_state=0)
+plt.scatter(X[:, 0], X[:, 1])
+plt.tight_layout()
+plt.show()
+
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 3))
+
+km = KMeans(n_clusters=2, random_state=0)
+y_km = km.fit_predict(X)
+ax1.scatter(X[y_km == 0, 0], X[y_km == 0, 1],
+            edgecolor='black',
+            c='lightblue', marker='o', s=40, label='cluster 1')
+ax1.scatter(X[y_km == 1, 0], X[y_km == 1, 1],
+            edgecolor='black',
+            c='red', marker='s', s=40, label='cluster 2')
+ax1.set_title('K-means clustering')
+
+ac = AgglomerativeClustering(n_clusters=2,
+                             affinity='euclidean',
+                             linkage='complete')
+y_ac = ac.fit_predict(X)
+ax2.scatter(X[y_ac == 0, 0], X[y_ac == 0, 1], c='lightblue',
+            edgecolor='black',
+            marker='o', s=40, label='Cluster 1')
+ax2.scatter(X[y_ac == 1, 0], X[y_ac == 1, 1], c='red',
+            edgecolor='black',
+            marker='s', s=40, label='Cluster 2')
+ax2.set_title('Agglomerative clustering')
+
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+db = DBSCAN(eps=0.2,
+            min_samples=5,
+            metric='euclidean')
+y_db = db.fit_predict(X)
+plt.scatter(X[y_db == 0, 0],
+            X[y_db == 0, 1],
+            c='lightblue',
+            edgecolors='black',
+            marker='o',
+            s=40,
+            label='Cluster 1')
+
+plt.scatter(X[y_db == 1, 0],
+            X[y_db == 1, 1],
+            c='red',
+            edgecolors='black',
+            marker='s',
+            s=40,
+            label='Cluster 2')
+
+plt.legend()
 plt.tight_layout()
 plt.show()
