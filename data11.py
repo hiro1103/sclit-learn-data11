3a92edf2b95de149944eb1c58e9c3859dc4cc215
murakami <96195951+hiro1103@users.noreply.github.com>
2022-04-17 00:13:19 +0900

    Add files via upload

--- data11.py
+++ data11.py
@@ -1,9 +1,10 @@
 from cProfile import label
-from operator import le
-from re import X
 from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
+from matplotlib import cm
+from sklearn.metrics import silhouette_samples
 import matplotlib.pyplot as plt
+import numpy as np
 
 # データ点の個数：n_samples
 # 特徴量の個数：n_features
@@ -63,6 +64,99 @@ plt.scatter(km.cluster_centers_[:, 0],
             c='red', edgecolor='black',
             label='Centroids')
 plt.legend(scatterpoints=1)
+plt.grid()
+plt.tight_layout()
+plt.show()
+
+print('Distortion: %.2f' % km.inertia_)
+
+distortions = []
+for i in range(1, 11):
+    km = KMeans(n_clusters=i,
+                init='k-means++',
+                n_init=10,
+                max_iter=300,
+                random_state=0)
+    km.fit(X)
+    distortions.append(km.inertia_)
+
+plt.plot(range(1, 11), distortions, marker='o')
+plt.xlabel('Number of clusters')
+plt.ylabel('Distortion')
+plt.tight_layout()
+plt.show()
+
+km = KMeans(n_clusters=3,
+            init='k-means++',
+            n_init=10,
+            max_iter=300,
+            tol=1e-04,
+            random_state=0)
+
+cluster_labels = np.unique(y_km)
+n_clusters = cluster_labels.shape[0]
+# シルエット係数を計算
+silhouette_vals = silhouette_samples(X, y_km, metric='euclidean')
+y_ax_lower, y_ax_upper = 0, 0
+yticks = []
+for i, c in enumerate(cluster_labels):
+    c_silhouseette_vals = silhouette_vals[y_km == c]
+    c_silhouseette_vals.sort()
+    y_ax_upper += len(c_silhouseette_vals)
+    # 色の値をセット
+    color = cm.jet(float(i)/n_clusters)
+    plt.barh(range(y_ax_lower, y_ax_upper),
+             c_silhouseette_vals,
+             height=1.0,
+             edgecolor='none',
+             color=color)
+    # クラスタラベルの表示位置を追加
+    yticks.append(y_ax_lower + y_ax_upper / 2.0)
+    # 底辺に値に棒の幅を追加
+    y_ax_lower += len(c_silhouseette_vals)
+
+# シルエット係数の平均値
+silhouette_avg = np.mean(silhouette_vals)
+# 係数の平均値に破線を引く
+plt.axvline(silhouette_avg, color="red", linestyle="--")
+# クラスタラベルを表示
+plt.yticks(yticks, cluster_labels+1)
+plt.ylabel('Cluster')
+plt.xlabel('Silhouette coefficient')
+plt.tight_layout()
+plt.show()
+
+km = KMeans(n_clusters=2,
+            init='k-means++',
+            n_init=10,
+            max_iter=300,
+            tol=1e-04,
+            random_state=0)
+y_km = km.fit_predict(X)
+plt.scatter(X[y_km == 0, 0],
+            X[y_km == 0, 1],
+            s=50,
+            c='lightgreen',
+            edgecolors='black',
+            marker='s',
+            label='Cluster 1')
+
+plt.scatter(X[y_km == 1, 0],
+            X[y_km == 1, 1],
+            s=50,
+            c='orange',
+            edgecolors='black',
+            marker='o',
+            label='Cluster 2')
+
+plt.scatter(km.cluster_centers_[:, 0],
+            km.cluster_centers_[:, 1],
+            s=250,
+            marker='*',
+            c='red',
+            label='Centroids')
+
+plt.legend()
 plt.grid()
 plt.tight_layout()
 plt.show()
